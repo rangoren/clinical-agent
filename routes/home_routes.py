f482dbacf2cedc1b1373b60c35d21bc4af72c74c
Ran Goren <ran@Rans-MacBook-Air.local>
2026-03-25 00:06:37 +0200

    Bump version to v0.1.30

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-APP_VERSION = "v0.1.29"
+APP_VERSION = "v0.1.30"
 
 
 @router.get("/")
