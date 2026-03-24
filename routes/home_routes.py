77a31306f162b757612f3c18e40da8cefe08983d
Ran Goren <ran@Rans-MacBook-Air.local>
2026-03-25 00:28:03 +0200

    Show updated date in source cards and bump version to v0.1.35

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-APP_VERSION = "v0.1.34"
+APP_VERSION = "v0.1.35"
 
 
 @router.get("/")
