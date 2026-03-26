8e733c4f6e84cf6681ac89105c7979f7d4ba4345
Ran Goren <ran@Rans-MacBook-Air.local>
2026-03-26 21:08:32 +0200

    Show study cards on every app entry in clinical mode

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-APP_VERSION = "v0.2.77"
+APP_VERSION = "v0.2.78"
 
 
 @router.get("/")
